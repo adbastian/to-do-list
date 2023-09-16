32c832e0b12d7a3e3c473db75b2955c4b92fbec4
Adi Bastian <abastian601@gmail.com>
2023-09-16 10:22:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import {FaPlus} from 'react-icons/fa'
+import {BsFileEarmarkCheckFill} from 'react-icons/bs'
+import { useState } from 'react';
+
+function App() {
+
+const [data, setData] = useState([])
+
+const onSubmit = (event) => {
+  event.preventDefault()
+
+  const form = event.target
+  const input = form.nama
+
+  console.log(input.value)
+
+  const dataList = [data, input.value].flat();
+  setData(dataList)
+
+  
+  console.log("data list", dataList)
+  console.log("data", data)
+
+  form.reset()
+}
+
+const onDeleteCart = (value) => {
+
+  const newData = data.filter((val) => val !== value)
+  setData(newData)
+}
+
+  return (
+ <>
+  <main>
+    <h1>TODO <br/><span>LIST</span></h1>
+    <form onSubmit={onSubmit}>
+      <input type='text' name='nama' id='nama' placeholder='What Your To Do...' required={true} />
+      <button><FaPlus size={25}/></button>
+    </form>
+    <hr/>
+    <ul>
+      {data.map((value, i) => (
+        <li key={i} className={i % 2 === 0 ? 'li1' : 'li2'} >
+          <span>{value}</span>
+          <button onClick={() => onDeleteCart(value)} className={i % 2 === 0 ? 'btn1' : 'btn2'} ><BsFileEarmarkCheckFill size={17}/></button>
+        </li>
+      ))}
+    </ul>
+  </main>
+  <div className='nav'>
+          <button onClick={() => setData([])}>Clear All</button>
+      </div>
+ </>
+  );
+}
+
+export default App;
